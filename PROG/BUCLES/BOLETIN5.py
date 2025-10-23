b9d15eb90d35d33b864e88d1adb0aee29a60bace
mariopdls <marioporra9@gmail.com>
2025-10-23 19:33:38 +0200

    Updated

--- PROG/BUCLES/BOLETIN5.py
+++ PROG/BUCLES/BOLETIN5.py
@@ -1,12 +1,44 @@
-#EMPEZAR BOLETIN
+# #EMPEZAR BOLETIN
 
-#ej2
-num=int(input("Dime un número: "))
-cont=0
-cadena=""
-for i in range (0,num):
-    cont+=1
-    cadena+= str(cont) + ""
-    print (cadena)
+# #ej2
+# num=int(input("Dime un número: "))
+# cont=0
+# cadena=""
+# for i in range (0,num):
+#     cont+=1
+#     cadena+= str(cont) + ""
+#     print (cadena)
 
 #SEGUIR
+
+# #EJ3
+# x=(int(input("Dime el valor de x: ")))
+# terminos=(int(input("Dime el numero de terminos: ")))
+# potencia=1
+# suma=0
+# for i in range (terminos):
+#     if i % 2 == 0:
+#         termino = x ** potencia
+#     else:
+#         termino = -(x ** potencia)---
+#     potencia+=2
+#     suma+=terminos
+# print (suma)
+
+#ej4
+# num=int(input("Dime un número: "))
+# uno="1"
+# cadena= " "
+# contador=0
+# for i in range (num):
+#     cadena+= f"{uno} +" + " "
+#     contador+=int(uno)
+#     uno+="1"
+# print (cadena[:-2])
+
+# print (contador)
+
+#ej5
+
+num=int(input("Dime un número: "))
+num2=int(input("Dime un segundo número: "))
