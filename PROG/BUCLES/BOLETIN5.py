4556a9adcd8140a4f54237dbc6b8991c73d3d203
mariopdls <marioporra9@gmail.com>
2025-10-24 18:37:12 +0200

    Updated

--- PROG/BUCLES/BOLETIN5.py
+++ PROG/BUCLES/BOLETIN5.py
@@ -56,17 +56,57 @@
 
 #ej6
 
-# num= (int(input("Dime un número: ")))
+# n= (int(input("Dime un número: ")))
 
-# for i in range (0, num, -1):
+# while n != 1:
+#     print(n)  # imprime cada número en su propia línea
+#     if n % 2 == 0:
+#         n = n // 2
+#     else:
+#         n = 3 * n + 1
+# print (1)
 
 
-# #num/2 es par
-# #num*3+1 si es impar
+#     #13 es impar, 13 (num) hace else
+#     #siguiente vuelta num=40
+#     # ¿como guardo el 40?
+
+#ej7
+
+day=int (int(input("Dime el día: ")))
+month= (int(input("Dime el mes: ")))
+year= (int(input("Dime el año: ")))
 
 
-def nombre_accion () :
-    print ("hola")
-    return
+# if month==4 or month==6 or month==9 or month==11:
+#     while day<1 or day>30:
+#         day=int (int(input("Dime un día válido para este mes: ")))
+# elif month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
+#     while day<1 or day>30:
+#          day=int (int(input("Dime el un día válido para este mes: ")))
+# elif month==2:
+#     if year%400==0 or (year%4==0 and year%100!=0):
+#         while day <1 or day>29:
+#          day=int (int(input("Dime el un día válido para este mes. El año es bisiesto: ")))
+#     else:
+#         while day<1 or day>28:
+#          day=int (int(input("Dime el un día válido de febrero: ")))
 
-print (nombre_accion())
+
+if month==4 or month==6 or month==9 or month==11:
+    while day<1 or day>30:
+        day=int (int(input("Dime un día válido para este mes: ")))
+elif month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
+    while day<1 or day>31:
+         day=int (int(input("Dime el un día válido para este mes: ")))
+elif month==2:
+    if year%400==0 or (year%4==0 and year%100!=0):
+        while day <1 or day>29:
+         day=int (int(input("Dime el un día válido para este mes. El año es bisiesto: ")))
+    else:
+        while day<1 or day>28:
+         day=int (int(input("Dime el un día válido de febrero: ")))
+
+print (f"La fecha es {day}/{month}/{year}.")
+
+
--- PROG/FUNCIONES/prueba1.py
+++ PROG/FUNCIONES/prueba1.py
@@ -4,16 +4,16 @@
 #     resultado_modulo=parametro1%2
 #     return resultado_modulo
 
-# print (modulo(parametro1%2))
+# # print (modulo(parametro1%2))
 
-# assert (modulo(6)==0)
-# assert (modulo(7)==1)
+# # assert (modulo(6)==0)
+# # assert (modulo(7)==1)
 
-parametro1= int(input("Dime un parámetro: "))
-def division (parametro1):
-    resultado_div=parametro1//2
-    return resultado_div
-print (division(parametro1))
+# parametro1= int(input("Dime un parámetro: "))
+# def division (parametro1):
+#     resultado_div=parametro1//2
+#     return resultado_div
+# print (division(parametro1))
 
-assert (division(5)==2) #está bien
-assert (division(5)==1) #está mal
+# assert (division(5)==2) #está bien
+# assert (division(5)==1) #está mal
