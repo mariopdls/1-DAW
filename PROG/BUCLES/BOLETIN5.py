78aa175b876e2089c9c1bf5d99547e5ec95639aa
mariopdls <marioporra9@gmail.com>
2025-10-26 17:46:49 +0100

    Updated

--- PROG/BUCLES/BOLETIN5.py
+++ PROG/BUCLES/BOLETIN5.py
@@ -71,11 +71,11 @@
 #     #siguiente vuelta num=40
 #     # ¿como guardo el 40?
 
-#ej7
+# #ej7
 
-day=int (int(input("Dime el día: ")))
-month= (int(input("Dime el mes: ")))
-year= (int(input("Dime el año: ")))
+# day=int (int(input("Dime el día: ")))
+# month= (int(input("Dime el mes: ")))
+# year= (int(input("Dime el año: ")))
 
 
 # if month==4 or month==6 or month==9 or month==11:
@@ -93,20 +93,87 @@ year= (int(input("Dime el año: ")))
 #          day=int (int(input("Dime el un día válido de febrero: ")))
 
 
-if month==4 or month==6 or month==9 or month==11:
-    while day<1 or day>30:
-        day=int (int(input("Dime un día válido para este mes: ")))
-elif month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
-    while day<1 or day>31:
-         day=int (int(input("Dime el un día válido para este mes: ")))
-elif month==2:
-    if year%400==0 or (year%4==0 and year%100!=0):
-        while day <1 or day>29:
-         day=int (int(input("Dime el un día válido para este mes. El año es bisiesto: ")))
-    else:
-        while day<1 or day>28:
-         day=int (int(input("Dime el un día válido de febrero: ")))
+# if month==4 or month==6 or month==9 or month==11:
+#     while day<1 or day>30:
+#         day=int (int(input("Dime un día válido para este mes: ")))
+# elif month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
+#     while day<1 or day>31:
+#          day=int (int(input("Dime el un día válido para este mes: ")))
+# elif month==2:
+#     if year%400==0 or (year%4==0 and year%100!=0):
+#         while day <1 or day>29:
+#          day=int (int(input("Dime el un día válido para este mes. El año es bisiesto: ")))
+#     else:
+#         while day<1 or day>28:
+#          day=int(input("Dime el un día válido de febrero: "))
 
-print (f"La fecha es {day}/{month}/{year}.")
+# print (f"La fecha es {day}/{month}/{year}.")
 
+# #ahora añadir un dia mas
 
+# day+=1
+# if month == 4 or month == 6 or month == 9 or month == 11:
+#     if day > 30:
+#         day = 1
+#         month += 1
+# elif month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10:
+#     if day > 31:
+#         day = 1
+#         month += 1
+# elif month == 12: #diciembre aparte por el cambio del año
+#     if day > 31:
+#         day = 1
+#         month = 1
+#         year += 1
+# elif month == 2:
+#     if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
+#         limite = 29
+#     else:
+#         limite = 28
+#     if day > limite:
+#         day = 1
+#         month += 1
+
+# print(f"Día siguiente: {day}/{month}/{year}.")
+
+# #ej8
+
+# num=int(input("dime un número positivo mayor que 0: "))
+# i=1
+# termino1=0
+# termino2=1
+# acum=0
+# acum+=0
+# cadena= ""
+# while num <=0:
+#     num=int(input("dime un número positivo mayor que 0"))
+# else:
+#     for i in range (1,num+1):
+        
+#         cadena+= print(termino1)
+#         resultado=termino1+termino2
+#         termino1=termino2
+#         termino2=resultado
+
+#cada termino es igual a la suma de los dos anteriores
+
+#ej9: XD
+
+# ej10
+edad=int(input("Di la edad: "))
+
+dinero1=20
+dinero2=15
+dinerototal=0
+acum=0
+
+while edad<=0 or edad>100:
+    edad=int(input("Di una edad válida: "))
+else:
+    for i in range(2, edad + 1, 2):   # solo años pares
+        dinerototal = dinero1 + dinero2   # nuevo dinero recibido (como Fibonacci)
+        dinero1 = dinero2
+        dinero2 = dinerototal
+
+        acum += dinerototal       # sumamos lo que recibe este año
+        print(i, "recibe", dinerototal, "y acumula", acum)
