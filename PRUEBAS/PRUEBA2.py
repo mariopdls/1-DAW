8f22fa4bd22685a520349fbd638ee251947b1d3c
mariopdls <marioporra9@gmail.com>
2025-10-29 12:51:52 +0100

    Updated

---
+++ PRUEBAS/PRUEBA2.py
@@ -0,0 +1,62 @@
+hora=int(input("Dime la hora: "))
+minutos= int(input("Dime los minutos: "))
+segundos= int(input("Dime los segundos: "))
+
+#24 horas
+
+# if hora<0 or hora >23:
+#     print ("Hora inválida.")
+# elif minutos <0 or minutos>59:
+#     print ("Minutos inválidos")
+# elif segundos<0 or segundos>59:
+#     print ("Segundos inválidos")
+# else:
+#     print (f"Son las {hora}:{minutos}:{segundos}")
+#     if hora==23 and minutos==59 and segundos==59:
+#         hora=0
+#         minutos=0
+#         segundos=0
+#         print (f"Dentro de un segundo serán las {hora}:{minutos}:{segundos}")
+#     elif minutos==59 and segundos==59:
+#         hora=hora+1
+#         minutos=0
+#         segundos=0
+#         print (f"Dentro de un segundo serán las {hora}:{minutos}:{segundos}")
+#     elif segundos==59:
+#         minutos=minutos+1
+#         segundos=0
+#         segundos=0
+#         print (f"Dentro de un segundo serán las {hora}:{minutos}:{segundos}")
+#     else:
+#         segundos=segundos+1
+#         print (f"Dentro de un segundo serán las {hora}:{minutos}:{segundos}")
+
+#12 horas
+
+# if hora<0 or hora >12:
+#     print ("Hora inválida.")
+# elif minutos <0 or minutos>59:
+#     print ("Minutos inválidos")
+# elif segundos<0 or segundos>59:
+#     print ("Segundos inválidos")
+# else:
+#     print (f"Son las {hora}:{minutos}:{segundos}")
+#     if hora==12 and minutos==59 and segundos==59:
+#         hora=0
+#         minutos=0
+#         segundos=0
+#         print (f"Dentro de un segundo serán las {hora}:{minutos}:{segundos}")
+#     elif minutos==59 and segundos==59:
+#         hora=hora+1
+#         minutos=0
+#         segundos=0
+#         print (f"Dentro de un segundo serán las {hora}:{minutos}:{segundos}")
+#     elif segundos==59:
+#         minutos=minutos+1
+#         segundos=0
+#         segundos=0
+#         print (f"Dentro de un segundo serán las {hora}:{minutos}:{segundos}")
+#     else:
+#         segundos=segundos+1
+#         print (f"Dentro de un segundo serán las {hora}:{minutos}:{segundos}")
+
